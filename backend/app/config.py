60050567f82918f24d9cd48e4d823227a914755f
Dhanoush KESSAVANE <kessavanedhanoush@gmail.com>
2026-02-04 16:13:52 +0100

    Set new Pydantic syntax to valid test

--- backend/app/config.py
+++ backend/app/config.py
@@ -1,18 +1,17 @@
-from pydantic_settings import BaseSettings
+from pydantic_settings import BaseSettings, SettingsConfigDict
 
 class Settings(BaseSettings):
+    # New V2 syntax for configuration
+    model_config = SettingsConfigDict(env_file=".env", extra="ignore")
+
     # Database connection URL (defaults to localhost)
     mongodb_url: str = "mongodb://localhost:27017"
     
     # Name of the database to be created
     database_name: str = "network_generator_db"
 
-    #Logging settings : INFO or DEBUG
+    # Logging settings: INFO or DEBUG
     log_level: str = "INFO"
-
-    class Config:
-        # Specifies the environment file to load
-        env_file = ".env"
 
 # Instantiate the settings object for use in other files
 settings = Settings()
